700b43a023ce9d6d8c517cd885e7e6f592531fd8
superginger65 <superginger65@gmail.com>
2025-05-21 22:32:56 -0400

    Added and finished inner note sight reading generator
    Hell yeah

--- SightreadingApp-Test2/app.js
+++ SightreadingApp-Test2/app.js
@@ -1,44 +1,73 @@
-const TOTAL_BEGINNING_MEASURES = 2; // total images available in the pool
-const TOTAL_INTERMEDIATE_MEASURES = 8; // total images available in the pool
-const TOTAL_END_MEASURES = 1; // total images available in the pool
-const NUM_TO_DISPLAY = 8;
+const NOTES = ['A', 'B', 'D', 'G'];
 
-function getRandomIndices() {
-  const indices = new Set();
-  indices.add(Math.floor(Math.random() * TOTAL_BEGINNING_MEASURES) + 1); // beginning
-  while (indices.size < NUM_TO_DISPLAY - 1) {
-    indices.add(Math.floor(Math.random() * TOTAL_INTERMEDIATE_MEASURES) + 1);
-  }
-
-const result = Array.from(indices);
-result.push(Math.floor(Math.random() * TOTAL_END_MEASURES) + 1); // end
-return result;
+function getRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function displayRandomMeasures() {
-  const container = document.getElementById('imageContainer');
-  container.innerHTML = ''; // clear previous images
-  const indices = getRandomIndices();
+function generateMeasureImages(isFirst, isLast, isBreak, beats, meter) {
+  const measure = [];
+  let remaining = beats;
 
-  var i = 0;
-  while (i < indices.length) {
-    const img = document.createElement('img');
-    if (i === 0) {
-      img.src = `images/begin${indices[i]}.png`;
-      img.alt = `Measure ${indices[i]}`;
-      container.appendChild(img);
-      i++;
+  while (remaining > 0) {
+    let dur = Math.min(remaining, beats); 
+    let duration = getRandom([...Array(dur).keys()].map(i => i + 1));
+    let note = getRandom(NOTES);
+    let folder = ``;
+    if (duration === beats) {
+      folder = `images/${meter}/barline/${duration}/${note}.jpg`;
     }
     else {
-    img.src = `images/measure${indices[i]}.png`;
-    img.alt = `Measure ${indices[i]}`;
-    container.appendChild(img);
-    i++;
+      folder = `images/${meter}/internal/${duration}/${note}.jpg`;
     }
-    if (i == indices.length) {
-      img.src = `images/end${indices[i - 1]}.png`;
-      img.alt = `Measure ${indices[i - 1]}`;
-      container.appendChild(img);
+    measure.push(folder);
+    remaining -= duration;
+  }
+  measure[measure.length - 1] = measure[measure.length - 1].replace('/internal/', '/barline/');
+  if (isFirst) {
+    measure[0] = measure[0].replace('/internal/', '/start/');
+    measure[0] = measure[0].replace('/barline/', '/start/');
+    measure[0] = measure[0].replace(/\/[A-G]\.jpg$/, '.jpg');
+
+  }
+  if (isLast) {
+    measure[measure.length - 1] = measure[measure.length - 1].replace('/internal/', '/end/');
+    measure[measure.length - 1] = measure[measure.length - 1].replace('/barline/', '/end/');
+    measure[measure.length - 1] = measure[measure.length - 1].replace(/\/[A-G]\.jpg$/, '.jpg');
+  }
+  if (isBreak) {
+    measure[0] = measure[0].replace('/internal/', '/break/');
+    measure[0] = measure[0].replace('/barline/', '/break/');
+  }
+
+
+  return measure;
+}
+
+function generateActivity(meter) {
+  const beats = meter === '3-4' ? 3 : 4;
+  const row1 = document.getElementById('row1');
+  const row2 = document.getElementById('row2');
+  row1.innerHTML = '';
+  row2.innerHTML = '';
+
+  const rows = [row1, row2];
+
+  for (let row = 0; row < 2; row++) {
+    for (let i = 0; i < 4; i++) {
+      const isFirst = row === 0 && i === 0;
+      const isLast = row === 1 && i === 3;
+      const isBreak = row === 1 && i === 0;
+
+      let images;
+
+        images = generateMeasureImages(isFirst, isLast, isBreak, beats, meter);
+      
+
+      images.forEach(src => {
+        const img = document.createElement('img');
+        img.src = src;
+        rows[row].appendChild(img);
+      });
     }
   }
 }
